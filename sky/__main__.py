a9e72fb134895d1b13b1bf6c21f19d408d366154
Ivan <nexacoders2@gmail.com>
2026-04-08 12:32:14 +0530

    Update __main__.py

--- sky/__main__.py
+++ sky/__main__.py
@@ -1,17 +1,25 @@
+from pyrogram import Client
+import config
 import os
 import importlib
-from sky import sky
 
+sky = Client(
+    "skybot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN
+)
 
 def load_plugins():
-    for root, _, files in os.walk("sky"):
+    for root, _, files in os.walk("sky/plugins"):
         for file in files:
             if file.endswith(".py") and not file.startswith("__"):
                 module = os.path.join(root, file).replace("/", ".")[:-3]
                 importlib.import_module(module)
+                print(f"✅ Loaded: {module}")
 
-
-def start():
+if __name__ == "__main__":
+    print("🔌 Loading Plugins...\n")
     load_plugins()
-    print("✅ Bot Started")
+    print("\n🚀 Bot Started!\n")
     sky.run()
