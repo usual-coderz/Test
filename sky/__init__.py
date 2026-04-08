8467b6dba005df289405ac772dfb285acdcae02a
Ivan <nexacoders2@gmail.com>
2026-04-08 12:26:03 +0530

    Update __init__.py

--- sky/__init__.py
+++ sky/__init__.py
@@ -1,1 +1,9 @@
-# fucker Aura
+from pyrogram import Client
+import config
+
+SKY = Client(
+    "skybot",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN
+)
