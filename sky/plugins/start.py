7a73e80b53807e808eed3267ddb948b51596cd1a
Ivan <nexacoders2@gmail.com>
2026-04-08 13:16:57 +0530

    Update start.py

--- sky/plugins/start.py
+++ sky/plugins/start.py
@@ -1,9 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from sky import sky   # 👈 important
+from sky import sky
+from sky.keyboards.start import start_keyboard 
 
 @sky.on_message(filters.command("start") & filters.private)
 async def start_handler(client, message: Message):
     await message.reply_text(
-        f"🌐"
+        "🌐",
+        reply_markup=start_keyboard
     )
