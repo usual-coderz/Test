b4cb0869416dbdac208f6ef97f1fc20a9fb7bdd7
Ivan <nexacoders2@gmail.com>
2026-04-08 13:31:18 +0530

    Update start.py

--- sky/plugins/start.py
+++ sky/plugins/start.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from sky import sky
-from sky.keyboards.start import start_inline
+from sky.keyboards.start import start_button
 
 @sky.on_message(filters.command("start") & filters.private)
 async def start_handler(client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text="🌐",
-        reply_markup=start_inline
+        reply_markup=start_button()
     )
