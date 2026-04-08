fdde42261d55c4c1745e1f0edf8e9be40f35382d
Ivan <nexacoders2@gmail.com>
2026-04-08 12:35:13 +0530

    Update start.py

--- sky/plugins/start.py
+++ sky/plugins/start.py
@@ -1,9 +1,8 @@
-from sky import sky
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@sky.on_message(filters.command("start") & filters.private)
-async def start_handler(client, message: Message):
+@Client.on_message(filters.command("start") & filters.private)
+async def start_handler(client: Client, message: Message):
     await message.reply_text(
         f"Hello {message.from_user.first_name} 👋\n\nSky Bot is running! 🚀"
     )
