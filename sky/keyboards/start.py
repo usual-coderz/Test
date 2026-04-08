05151e268ca48442f0a7775e79bb3d6680ea56ce
Ivan <nexacoders2@gmail.com>
2026-04-08 13:15:18 +0530

    Update start.py

--- sky/keyboards/start.py
+++ sky/keyboards/start.py
@@ -3,22 +3,22 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ButtonSty
 start_inline = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton("🕊️ охота", callback_data="hunt"),
-            InlineKeyboardButton("🔥 арена", callback_data="arena"),
-            InlineKeyboardButton("🧰 инвентарь", callback_data="inventory"),
+            InlineKeyboardButton("охота", callback_data="hunt"),
+            InlineKeyboardButton("арена", callback_data="arena"),
+            InlineKeyboardButton("инвентарь", callback_data="inventory"),
         ],
         [
-            InlineKeyboardButton("🛡️ рейтинг", callback_data="rating"),
-            InlineKeyboardButton("📊 топ", callback_data="top"),
-            InlineKeyboardButton("📉 рынок", callback_data="market"),
+            InlineKeyboardButton("рейтинг", callback_data="rating"),
+            InlineKeyboardButton("топ", callback_data="top"),
+            InlineKeyboardButton("рынок", callback_data="market"),
         ],
         [
-            InlineKeyboardButton("✅ коллекция", callback_data="collection"),
-            InlineKeyboardButton("⭐ донат", callback_data="donate", style=ButtonStyle.PRIMARY),
-            InlineKeyboardButton("⚙️ настройки", callback_data="settings"),
+            InlineKeyboardButton("коллекция", callback_data="collection"),
+            InlineKeyboardButton("донат", callback_data="donate", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton("настройки", callback_data="settings"),
         ],
         [
-            InlineKeyboardButton("🌐 комьюнити", callback_data="community", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton("комьюнити", callback_data="community", style=ButtonStyle.PRIMARY),
         ],
     ]
 )
