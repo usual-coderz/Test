0a4b3d4ccc0f6c0296107e9cb5dff7dcab764940
Ivan <nexacoders2@gmail.com>
2026-04-08 12:32:30 +0530

    Update __init__.py

--- sky/__init__.py
+++ sky/__init__.py
@@ -1,9 +1,1 @@
-from pyrogram import Client
-import config
-
-sky = Client(
-    "skybot",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
-)
+from .__main__ import sky
