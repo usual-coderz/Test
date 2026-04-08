264cfb164cf2262e6512fe4fd9fffbad54a36ecc
Ivan <nexacoders2@gmail.com>
2026-04-08 12:27:26 +0530

    Create __main__.py

---
+++ sky/__main__.py
@@ -0,0 +1,17 @@
+import os
+import importlib
+from sky import sky
+
+
+def load_plugins():
+    for root, _, files in os.walk("sky"):
+        for file in files:
+            if file.endswith(".py") and not file.startswith("__"):
+                module = os.path.join(root, file).replace("/", ".")[:-3]
+                importlib.import_module(module)
+
+
+def start():
+    load_plugins()
+    print("✅ Bot Started")
+    sky.run()
